b4e3c07d86f50453b326f2b441cb67ecfccefff3
Antti L <a.lipsanen@gmail.com>
2018-08-18 15:31:22 +0300

    Added crossplatforminput and created basic horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] float runSpeed = 5f;
+
+
+	private Rigidbody2D playerRb;
+
+	// Use this for initialization
+	void Start () {
+		playerRb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Run();
+	}
+
+	private void Run() {
+		float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // between -1 and +1
+		Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, playerRb.velocity.y);
+		playerRb.velocity = playerVelocity;
+		}
+	}
