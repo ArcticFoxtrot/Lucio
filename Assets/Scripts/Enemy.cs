469ecbb43f968838bd193261215c0149843f57db
Antti L <a.lipsanen@gmail.com>
2018-08-19 11:18:50 +0300

    Added enemy and enemy movement and enemy animation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+	[SerializeField] float enemySpeed = 1f;
+
+	private Rigidbody2D enemyRb;
+	// Use this for initialization
+	void Start () {
+		enemyRb = gameObject.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsFacingRight()) {
+			enemyRb.velocity = new Vector2(-enemySpeed, 0);
+			} else {
+			enemyRb.velocity = new Vector2(enemySpeed, 0);
+			}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision) {
+		print("Triggered");
+		transform.localScale = new Vector2((Mathf.Sign(enemyRb.velocity.x)), 1f);
+		}
+
+	private bool IsFacingRight() {
+		return transform.localScale.x > 0;
+		}
+	}
+
+
